87a1d0c0a6dbb51a70c1821aa3b62d36359e0fed
sdjnduantong <15253130515@163.com>
2021-09-07 13:49:40 +0800

    返回到原始模式

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,6 +1,15 @@
-export default {
-  state: {},
-  actions: {},
-  mutations: {},
-  getters: {}
-}
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
