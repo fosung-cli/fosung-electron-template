3fb9e684dab9a03962c1a0df95a7b87fa879054c
sdjnduantong <15253130515@163.com>
2021-09-09 16:28:07 +0800

    feat(自动化流程): 自动化流程
    1. 自动化流程修改
    2. 执行命令修改
    
    Signed-off-by: sdjnduantong <15253130515@163.com>

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ git pull origin $master
 echo "Current pull origin $master."
 
 # Auto generate version number and tag
-standard-version -r $release --tag-prefix $prefix
+standard-version -p $release --tag-prefix $prefix
 
 git push --follow-tags origin $master
 
